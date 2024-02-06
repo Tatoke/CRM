f6153a07a127aea8c73b78d7ee68cca8e49a7e70
Inga Bezruchko <ingadezor@gmail.com>
2024-02-06 03:14:40 -0600

    set up App.css (global style), fonts and bootstrap
    Set up react-boostrap library and google fonts. Added global styles for the whole project to App.css file. Added background images.

--- Admin/frontend/src/App.jsx
+++ Admin/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 // main file of the frontend application that renders the components.
-
+import 'bootstrap/dist/css/bootstrap.min.css'; // !Put any other imports below so that CSS from your components takes precedence over default styles.
+import './App.css'        //styles for the WHOLE project
+import Navbar from './layouts/Navbar/Navbar'
 
 
 
@@ -13,11 +15,11 @@ function App() {
 
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <p>I was rendered inside App</p>
     </>
   )
 }
+
 
 export default App
---
+++ Admin/frontend/src/layouts/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css'
+
+
+
+
+function Navbar(){
+
+
+
+    return (
+        <div>
+            <p>Navbar</p>
+        </div>
+    )
+}
+
+
+
+
+export default Navbar
+
